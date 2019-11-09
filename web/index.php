00599ae8c7cd101bb1d98697b15f98363d77432a
Ralf Lang <lang@b1-systems.de>
2019-11-09 09:37:08 +0100

    If Horde is not activated, check if a wizard is present, otherwise give a hint

--- web/index.php
+++ web/index.php
@@ -1,2 +1,8 @@
 <?php
-require_once __DIR__ . '/horde/index.php';
+if (file_exists(__DIR__ . '/horde/config/conf.php')) {
+    require_once __DIR__ . '/horde/index.php';
+} elseif (file_exists(__DIR__ . '/wizard/index.php'))) {
+    require_once __DIR__ . '/wizard/index.php';
+} else {
+    printf("Please create a %s file to activate Horde", __DIR__ . '/horde/config/conf.php');
+}

a1da3be531c9c9a9836148d07650bd00c5bbfd54
Ralf Lang <lang@b1-systems.de>
2019-11-09 19:58:15 +0100

    syntax

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 if (file_exists(__DIR__ . '/horde/config/conf.php')) {
     require_once __DIR__ . '/horde/index.php';
-} elseif (file_exists(__DIR__ . '/wizard/index.php'))) {
+} elseif (file_exists(__DIR__ . '/wizard/index.php')) {
     require_once __DIR__ . '/wizard/index.php';
 } else {
     printf("Please create a %s file to activate Horde", __DIR__ . '/horde/config/conf.php');

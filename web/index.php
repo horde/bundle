994adaf5052ab289c3da08dffc755dd8ee02c56d
Ralf Lang <ralf.lang@ralf-lang.de>
2025-06-03 15:22:17 +0000

    Use var/config/ url for redirect page

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
-if (file_exists(__DIR__ . '/horde/config/conf.php')) {
+if (file_exists(dirname(__DIR__) . '/var/config/horde/conf.php')) {
     require_once __DIR__ . '/horde/index.php';
 } elseif (file_exists(__DIR__ . '/wizard/index.php')) {
     require_once __DIR__ . '/wizard/index.php';
 } else {
-    printf("Please create a %s file and then run 'composer horde-reconfigure' to activate Horde", dirname(__DIR__) . '/var/config/horde/conf.php');
+    printf("Please create a %s file and then run 'composer horde:reconfigure' to activate Horde", dirname(__DIR__) . '/var/config/horde/conf.php');
 }
